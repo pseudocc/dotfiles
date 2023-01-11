8a342b42ca1147ba4c69746ddcae2cbf8fc78701
pseudoc <atlas.yu@canonical.com>
2023-01-11 10:33:45 +0800

    preference changes

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -3,6 +3,9 @@ local wrap = require('utils').wrap
 
 local no_remap = { remap = false }
 
+-- most import remaps
+map.set('i', '<S-Tab>', [[<C-V><Tab>]])
+
 -- move lines up/down or character left/right
 map.set('n', '<M-k>', [[:m .-2<CR>==]], no_remap)
 map.set('n', '<M-j>', [[:m .+1<CR>==]], no_remap)
@@ -15,9 +18,11 @@ map.set('i', '<M-h>', [[<Esc>"9x"9phi]], no_remap)
 map.set('v', '<M-k>', [[:m '<-2<CR>gv=gv]], no_remap)
 map.set('v', '<M-j>', [[:m '>+1<CR>gv=gv]], no_remap)
 
+-- misc
 map.set("n", "<leader>f", vim.lsp.buf.format, no_remap)
 map.set('n', '<leader>W', [[:w !sudo tee % > /dev/null<CR>]], no_remap)
 
+-- buffer navigations
 map.set('n', '<leader>[', vim.cmd.bprevious, no_remap)
 map.set('n', '<leader>]', vim.cmd.bnext, no_remap)
 map.set('n', '<leader><leader>', vim.cmd.ball, no_remap)

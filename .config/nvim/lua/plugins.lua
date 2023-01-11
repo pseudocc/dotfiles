a20d8d3bcb635df0e5f26bd2fefa533785b28dc1
pseudoc <atlas.yu@canonical.com>
2023-01-11 17:46:55 +0800

    add and configure gitsigns

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -120,6 +120,52 @@ return require('packer').startup(function (use)
     end
   }
 
+  use {
+    'lewis6991/gitsigns.nvim',
+    config = function ()
+      require('gitsigns').setup {
+        signcolumn = false,
+        numhl = true,
+        on_attach = function (bufnr)
+          local gs = package.loaded.gitsigns
+
+          local function map(mode, l, r, opts)
+            opts = opts or { remap = false }
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          map('n', '[c', function ()
+            if vim.wo.diff then
+              return '[c'
+            end
+            vim.schedule(gs.next_hunk)
+            return '<Ignore>'
+          end, { expr = true })
+
+          map('n', ']c', function ()
+            if vim.wo.diff then
+              return ']c'
+            end
+            vim.schedule(gs.next_hunk)
+            return '<Ignore>'
+          end, { expr = true })
+
+          -- Actions
+          map({ 'n', 'v' }, '<leader>hs', [[:Gitsigns stage_hunk<CR>]])
+          map({ 'n', 'v' }, '<leader>hr', [[:Gitsigns reset_hunk<CR>]])
+          map('n', '<leader>hu', gs.undo_stage_hunk)
+          map('n', '<leader>hp', gs.preview_hunk)
+          map('n', '<leader>tb', gs.toggle_current_line_blame)
+          map('n', '<leader>hd', gs.diffthis)
+
+          -- Motion
+          map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+        end
+      }
+    end
+  }
+
   -- Completion tools
   use {
     'windwp/nvim-autopairs',

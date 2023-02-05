607fc6692e6d66a70f191a928bc75fb6da4b976c
pseudoc <atlas.yu@canonical.com>
2023-02-05 11:07:04 +0800

    update nvim-tree config(open at startup).
    see: https://github.com/nvim-tree/nvim-tree.lua/wiki/Open-At-Startup

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -1,6 +1,24 @@
 local map = vim.keymap
 local no_remap = { remap = false }
 
+local function open_nvim_tree(data)
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not directory then
+    return
+  end
+
+  -- change to the directory
+  vim.cmd.cd(data.file)
+
+  -- open the tree
+  require('nvim-tree.api').tree.open()
+end
+
+vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = open_nvim_tree })
+
 map.set('n', '<C-b>', vim.cmd.NvimTreeToggle, no_remap)
 map.set('n', '<leader>n', vim.cmd.NvimTreeFocus, no_remap)
 map.set('n', '<C-f>', vim.cmd.NvimTreeFindFile, no_remap)
+

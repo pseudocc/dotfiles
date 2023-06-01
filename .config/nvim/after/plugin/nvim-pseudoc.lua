ce621d7c1737c4eb4b7cd354551dd78bb52e9123
pseudoc <atlas.yu@canonical.com>
2023-06-01 19:02:50 +0800

    motion for zig `for (values) |value|`

--- .config/nvim/after/plugin/nvim-pseudoc.lua
+++ .config/nvim/after/plugin/nvim-pseudoc.lua
@@ -4,3 +4,9 @@ vim.keymap.set({ 'o', 'x' }, 'ic',
   [[:<C-u>lua require('pseudoc.motions').select_context(false)<CR>]], opts)
 vim.keymap.set({ 'o', 'x' }, 'ac',
   [[:<C-u>lua require('pseudoc.motions').select_context(true)<CR>]], opts)
+
+vim.keymap.set({ 'o', 'x' }, 'i|',
+  [[:<C-u>lua require('pseudoc.motions').arbitrary_motion('|', false)<CR>]], opts)
+vim.keymap.set({ 'o', 'x' }, 'a|',
+  [[:<C-u>lua require('pseudoc.motions').arbitrary_motion('|', true)<CR>]], opts)
+

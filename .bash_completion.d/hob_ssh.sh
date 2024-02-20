8a7c932567dd36d0910cb9e81e1f46f285674395
pseudoc <atlas.yu@canonical.com>
2024-02-20 13:48:44 +0800

    hob_ssh

---
+++ .bash_completion.d/hob_ssh.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+_hob_ssh() {
+  local cur prev ssh mock opts
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
+  mock="${MOCK:-u@mock.local}"
+  ssh="${SSH_NC:-ssh}"
+  opts=$(${ssh} "${mock} hob_devices")
+  COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+  return 0
+}
+
+complete -F _hob_ssh hob_ssh
